06525e3161229f8e1a4bf9e391dbcec541feab63
Peter Tucker <ptucker@whitworth.edu>
2023-01-04 11:00:24 -0800

    Tried to answer question comments in code
    My responses are prefixed with PT --. I've given some code examples that may not be perfect, but are hopefully helpful.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ class Main extends React.Component {
         const {regions} = this.state //Takes list of regions from \getregions server
         var url = this.urlbase + '/getregions' //Accesses server
         axios.get(url).then((resp) => { //???
+            //PT -- This code runs after you get the results from getregions. In this case, you'll take what's in data,
+            // which I think is a list of regions, and store it in your state. Notice in render() you get regions from
+            // state so they can be displayed in the regions-select drop-down
             console.log(resp)
             this.setState({...this.state, 
                 regions: resp.data,
@@ -25,9 +28,12 @@ class Main extends React.Component {
         var url2 = this.urlbase2 + '/getcountries' //Accesses server
         //var regionid_url = this.urlbase_regionid + '/getregionid'
         axios.get(url2/*, regionid_url*/).then((resp) => { //???
+            //PT -- as above, you're getting a list of countries from your Python server. Here you might
+            // put the code to get a random index
             console.log(resp)
             this.setState({...this.state,
                 countries: resp.data,
+                //PT -- maybe add this: country_index = (Math.random() * countries.length).parseInt()
             })
         }).catch(error => { //Catch errors, tell in console in dev tools
             console.log(error)
@@ -38,13 +44,16 @@ class Main extends React.Component {
         super()
         this.urlbase = 'http://127.0.0.1:5000'     // localhost
         // this.urlbase = 'https://flask-service.2346o2l3anjri.us-west-2.cs.amazonlightsail.com'
-        this.state = {ID: '', regions: []}
+        this.state = {ID: '', regions: []}  //PT -- you change state two lines later, so you don't need this.
         this.urlbase2 = 'http://127.0.0.1:5000'     // localhost
         this.state = {regionid: -1, regions: [], countryid: -1, countries: []} //Links together country and regionID information
+        //PT -- you might set the country index in the above line also:
+        //PT -- this.state = {regionid: -1, regions: [], countryid: -1, countries: [], country_index: -1} //Links together country and regionID information
         //TRYING TO PRINT RANDOM COUNTRY: var country_index = (Math.random() * this.state.length).parseInt()
     }
 
     componentDidMount() { //???
+        //PT -- this gets called automatically once your website is loaded for the first time. You fetch the regions here
         debugger
         this.getRegions()
     }
@@ -58,12 +67,16 @@ class Main extends React.Component {
     onRegionChange(e) { 
         //This passes the chosen region id to the server
         var url = this.urlbase + '/getcountries/'+e.target.value //???
+        //PT -- e has the information about the region drop-down. e.target.value has the chosen region, which you send to your server
+        //   so you can get the countries in that region
         console.log(url) //??
         //Places region list into combobox, then allows you to  choose one reion out of those in the combobox
         axios.get(url/*, regionid_url*/).then((resp) => { //???
             console.log(resp)
             this.setState({...this.state,
                 countries: resp.data,
+                //PT -- as before, you might add this line to set the country index:
+                //PT -- maybe add this: country_index = (Math.random() * resp.data.length).parseInt()
             })
         }).catch(error => { //Catch error, tell console
 
@@ -73,6 +86,7 @@ class Main extends React.Component {
 
     //Put countries into a list?
     onCountryChange(e) {
+        //PT -- since you're only getting one country, it won't change. You won't need this.
         this.state = { countries: [] }
     }
 
@@ -80,10 +94,14 @@ class Main extends React.Component {
 
     //What the website actually shows
     render () {
+        //PT -- this gets the current list of regions and countries from state. You might also get the country index:
+        // const {regions, countries, country_index} = this.state //???
         const {regions, countries} = this.state //???
         const optregions = regions.map((r)=>{ //???
+            //PT -- this builds an list of options in the region drop-down.
             return <option key={r.id} value={r.id}>{r.region}</option>
         })
+        //PT -- since you only want to show one country, you probably don't need this.
         const optcountries = countries.map((r)=>{ //???
             return <span key={r.id} value={r.id}>{r.country}</span>
         })
@@ -100,6 +118,8 @@ class Main extends React.Component {
             </div>
             <div className='countries-group'>
                 <label htmlFor="">Country: </label>
+                //PT -- you only want to show one country. So this might work:
+                // { country_index >= 0 && <span className='countries'>{countries[country_index]}</span>
                 <span className='countries'>{optcountries}</span>
             </div>
         </div>)
@@ -108,5 +128,6 @@ class Main extends React.Component {
     
 }
  //???
+//PT -- this simply calls your Main class (which you define above) to build the website.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Main />);
